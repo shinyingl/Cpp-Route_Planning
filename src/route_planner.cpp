b7743487f9dd9d3ded6832ca8870d0b0bf742709
Shin-Ying Lu <shinyingl@gmail.com>
2020-02-03 22:25:51 -0800

    fix the bug in the vector order in 'ConstructFinalPath'

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -82,17 +82,18 @@ std::vector<RouteModel::Node> RoutePlanner::ConstructFinalPath(RouteModel::Node 
     distance = 0.0f;
     std::vector<RouteModel::Node> path_found;
     RouteModel::Node parent;
-    // path_found.insert(*current_node);
-    path_found.emplace_back(*current_node);
+    
     // TODO: Implement your solution here.
     while (current_node-> parent != nullptr){
-        // path_found.insert(*current_node);
-        path_found.emplace_back(*current_node);
+        path_found.insert(path_found.begin(),*current_node);
+        // path_found.push_back(*current_node);
         parent = *(current_node-> parent);
         distance += current_node-> distance(parent); 
         current_node = current_node->parent;
     }
-    // path_found.push_back(*current_node);
+    path_found.insert(path_found.begin(),*current_node);
+    // path_found.push_back(*current_node);   
+    // std::reverse(path_found.begin(), path_found.end());
     distance *= m_Model.MetricScale(); // Multiply the distance by the scale of the map to get meters.
     return path_found;
 
